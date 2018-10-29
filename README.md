5596fc3083ed2ff23093801215368cb78e16908a
Josias Alexandre Oliveira <josiasalexandre@gmail.com>
2018-10-28 23:49:56 -0300

    script inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+Script em python 3 para converter uma base com imagens com tamanho superior a 1242x375 para esse formato.
+
+Uso:
+    $ pyhton3 caminho/da/pasta/de/origem caminho/da/pasta/de/destino
+
+O script copiará todas as imagens da pasta de origem (primeiro argumento), fazendo as transformações nas imagens e tudo será salvo na pasta de destino.
+
+Obs: todas as imagens serão convertidas para o formato .png!
---
+++ imageimp.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+import PIL
+from PIL import Image
+
+def cropscale_image(source_image, nw = 1242, nh = 375):
+  img = Image.open(source_image)
+  if img.size[0] != nw:
+    wpercent = (nw /float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((nw,hsize), PIL.Image.ANTIALIAS)
+  if img.size[1] != nh:
+    h2 = img.size[1] / 2
+    srow = int(h2 - nh / 2)
+    img = img.crop((0, srow, img.size[0], srow + nh))
+  return img
+
+def main(source, destination):
+  for filename in os.listdir(source):
+    if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".tiff"):
+      nimg = cropscale_image(source_image = filename, nw = 1242, nh = 375)
+      nimg.save(destination + filename)
+
+if __name__ == "__main__":
+  if 2 < len(sys.argv):
+    main(sys.argv[1],sys.argv[2])
+  else:
+    print("usage: python3 source/folder/path/ destination/folder/path/\n")
