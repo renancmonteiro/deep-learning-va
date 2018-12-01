b39157156b187cf254f7c6abd24ce50bcf56113c
Josias Alexandre Oliveira <josiasalexandre@gmail.com>
2018-11-30 23:12:04 -0200

    smaller images

--- imageimp.py
+++ imageimp.py
@@ -4,8 +4,7 @@ import os
 import PIL
 from PIL import Image
 
-def cropscale_image(source_image, nw = 1242, nh = 375):
-  img = Image.open(source_image)
+def cropscale_image(img, nw = 1242, nh = 375):
   if img.size[0] != nw:
     wpercent = (nw /float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
@@ -21,11 +20,15 @@ def main(source, destination):
     source += '/'
   if not destination.endswith('/'):
     destination += '/'
-  for filename in os.listdir(source):
-    if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".tiff"):
-      nimg = cropscale_image(source_image = source + filename, nw = 1242, nh = 375)
-      nimg.save(destination + filename)
-      nimg.close()
+  with open('smaller.txt', 'w') as h:
+    for filename in os.listdir(source):
+      if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".tiff"):
+        img = Image.open(source + filename)
+        if img.size[0] < 1242:
+          h.write("%s: %d %d\n" % (filename, img.size[0], img.size[1]))
+        nimg = cropscale_image(img, nw = 1242, nh = 375)
+        nimg.save(destination + filename)
+        nimg.close()
 
 if __name__ == "__main__":
   if 2 < len(sys.argv):
